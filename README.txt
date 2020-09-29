33d4090834cee2cb4faa6b5ea55d87fc11e521b6
Caleb W <51061101+calebrwalk5@users.noreply.github.com>
2020-09-28 21:54:57 -0400

    Add files via upload

---
+++ README.txt
@@ -0,0 +1,13 @@
+https://github.com/calebrwalk5
+https://github.com/mig-hub/mikeOS
+
+Path Operating System is a 16-bit operating system written in assembly and basic. PathOS uses the MikeOS API for many of its functions. 
+
+How to install: 
+Put PathOS.iso in a VM or on a USB stick. If you are reading this, you know how to do that.
+You can also write PathOS.flp on a floppy.
+
+How to build disk image:
+sudo su
+chmod +x build.sh
+./build.sh
---
+++ build.sh
@@ -0,0 +1,73 @@
+#!/bin/sh
+
+# This script assembles the PathOS bootloader, kernel and programs
+# (If you need to blank the floppy image: 'mkdosfs image/PathOS.flp')
+
+
+if test "`whoami`" != "root" ; then
+	echo "You must be logged in as root to build (for loopback mounting)"
+	echo "Enter 'su' or 'sudo bash' to switch to root"
+	exit
+fi
+
+
+if [ ! -e image/PathOS.flp ]
+then
+	echo ">>> Creating new PathOS floppy image..."
+	mkdosfs -C image/PathOS.flp 1440 || exit
+fi
+
+
+echo ">>> Assembling bootloader..."
+
+nasm -O0 -w+orphan-labels -f bin -o src/boot/boot.bin src/boot/bootload.asm || exit
+
+
+echo ">>> Assembling PathOS kernel..."
+
+cd src
+nasm -O0 -w+orphan-labels -f bin -o kernel.bin kernel.asm || exit
+cd ..
+
+
+echo ">>> Assembling programs..."
+
+cd programs
+
+for i in *.asm
+do
+	nasm -O0 -w+orphan-labels -f bin $i -o `basename $i .asm`.bin || exit
+done
+
+cd ..
+
+
+echo ">>> Adding bootloader to floppy image..."
+
+dd status=noxfer conv=notrunc if=src/boot/boot.bin of=image/PathOS.flp || exit
+
+
+echo ">>> Copying PathOS kernel and programs..."
+
+rm -rf tmp-loop
+
+mkdir tmp-loop && mount -o loop -t vfat image/PathOS.flp tmp-loop && cp src/kernel.bin tmp-loop/
+
+cp programs/*.bin programs/*.bas programs/sample.pcx tmp-loop
+
+sleep 0.2
+
+echo ">>> Unmounting loopback floppy..."
+
+umount tmp-loop || exit
+
+rm -rf tmp-loop
+
+
+echo ">>> Creating CD-ROM ISO image..."
+
+rm -f image/PathOS.iso
+mkisofs -quiet -V 'PathOS' -input-charset iso8859-1 -o image/PathOS.iso -b PathOS.flp image/ || exit
+
+echo '>>> Done!'
+
