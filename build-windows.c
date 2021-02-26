a07f27dda77ce600a3b7632ecef9aba6a27e05f2
Caleb W <51061101+calebrwalk5@users.noreply.github.com>
2021-02-26 09:14:11 -0500

    Update build-windows.c

--- build-windows.c
+++ build-windows.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 #include <Windows.h>
 
+// This script is in development, it might not work for you!
+
 int main() {
   printf("PathOS Build Script For Windows\n");
   printf("Assembling bootloader\n");
