9598c24e6c98dfb30460c5a6565ab0b3d9eef518
Caleb W <51061101+calebrwalk5@users.noreply.github.com>
2021-02-26 11:55:12 -0500

    Update build-windows.c

--- build-windows.c
+++ build-windows.c
@@ -8,7 +8,7 @@ int main() {
   printf("Assembling bootloader\n");
   system("cd src/boot/ & nasm -00 -f bin -o bootload.bin bootload.bin");
   printf("Assembling PathOS kernel\n");
-  system("cd src & nasm -00 -f -o kernel.bin kernel.asm");
+  system("cd clib & nasm -00 -f -o kernel.bin kernel.asm");
   printf("Compiling libraries\n");
   system("cd src & nasm -f elf -o ..\mlib.a mlib.asm");
   return 0;
